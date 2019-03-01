593a3806f70cae3b5f532774ca164f19a6eb5fb1
LeoShestakov <leo.shestakov@hotmail.com>
2019-03-01 14:02:44 -0500

    v3

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,6 +1,6 @@
 public class UserOfSpeakers {
     public static void main(String[] commandLine) {
-	Animal retriever = new Retrierver();
+	Animal retriever = new Retriever();
 	System.out.println(retriever.speak());
     }
 }
